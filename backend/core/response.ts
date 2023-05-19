b0aeaa9297f5cfca7f6570037958cdec46b1e0c8
Jibao Mansaray <jibaomansaray@gmail.com>
2023-05-19 17:08:24 +1000

    Add a response helper to makes JSON responses

--- backend/core/response.ts
+++ backend/core/response.ts
@@ -4,3 +4,11 @@ export function makeHttpResponse (body: string): Response {
 
   return response;
 }
+
+
+export function makeJSONResponse (body: unknown): Response {
+  const response = new Response(JSON.stringify(body));
+  response.headers.set('content-type', 'application/json');
+
+  return response;
+}
