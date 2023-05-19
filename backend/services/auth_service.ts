e5bac8041d4e83d914bc90ff781c88d79dfcc9fc
Jibao Mansaray <jibaomansaray@gmail.com>
2023-05-19 17:10:56 +1000

    Start work on the `auth` service

--- backend/services/auth_service.ts
+++ backend/services/auth_service.ts
@@ -1,4 +1,8 @@
+import { makeHttpResponse } from "../core/response.ts";
 import { HTTPRequest } from "../core/router.ts";
+import { ITenant, TenantId } from "../repository/tenant_repository.ts";
+import { IUser } from "../repository/user_repository.ts";
+import { ApiError, makeApiHttpResponse, makeApiResponse } from "./api_service.ts";
 
 export function isAuthenticated () {
 
@@ -16,4 +20,34 @@ export function getTokenFromQueryString (req: HTTPRequest): string | undefined {
 
 export function getAccessToken (req: HTTPRequest) {
   return getTokenFromHeader(req) || getTokenFromQueryString(req)
+}
+
+export function isAdmin (token?: string): boolean {
+  if (!token) {
+    return false;
+  }
+
+  // 1. Find the admin user by token
+  // 2. If user exist and the account is active, return true else false
+
+  return "abcd123" === token;
+}
+
+export function isTenant (): boolean {
+  return false;
+}
+
+export function isGuest (): boolean {
+  return (!isAdmin() && !isTenant());
+}
+
+export function generateUserToken (tenant: ITenant | TenantId): string {
+  const id = (typeof tenant === 'object') ? tenant.id as TenantId : tenant;
+  return `${id}.${crypto.randomUUID().replaceAll('-', '')}`
+}
+
+export function makePermissionDeniedResponse (): Response {
+  return makeApiHttpResponse(false, undefined, new ApiError('Permission denied', {
+    cause: 'token_wrong'
+  }))
 }
