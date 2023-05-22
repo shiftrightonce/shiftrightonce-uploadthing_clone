decfe2b9935a660dddd4523b5a73636d0c1a5f74
Jibao Mansaray <jibaomansaray@gmail.com>
2023-05-22 23:51:18 +1000

    Add a helper to inject the current user record and token

--- backend/routes.ts
+++ backend/routes.ts
@@ -4,10 +4,21 @@ import { ServerController } from "./controllers/server_controller.ts";
 import { registerTenantController } from "./controllers/tenant/tenant_controller.ts";
 import { UploadController } from "./controllers/upload_controller.ts";
 import { HTTPRequest, RouteManager } from "./core/router.ts";
-import { getAccessToken, isAdmin, makePermissionDeniedResponse } from "./services/auth_service.ts";
+import { UserTenant } from "./entities/user_tenant_entity.ts";
+import { canUpload, fetchUserTenant, getAccessToken, isAdmin, isSysAdmin, makePermissionDeniedResponse } from "./services/auth_service.ts";
 
 const router = new RouteManager();
 
+async function enrichRequest (req: HTTPRequest): Promise<UserTenant | false> {
+  const token = getAccessToken(req);
+  const userTenant = await fetchUserTenant(token);
+
+  req.addData('token', token);
+  req.addData('userTenant', userTenant);
+
+  return userTenant;
+
+}
 
 // general
 router.get("/", (_req: HTTPRequest) => {
@@ -31,27 +42,22 @@ const serverController = new ServerController();
 router.group('/v1/admin', (r) => {
   registerAdminTenantRoutes(r);
   registerAdminUserRoutes(r);
-}).registerMiddleware((req, next) => {
-  const token = getAccessToken(req);
-  req.addData('token', token);
-
-  return (isAdmin(token)) ? next(req) : makePermissionDeniedResponse();
+}).registerMiddleware(async (req, next) => {
+  return (isSysAdmin(await enrichRequest(req))) ? next(req) : makePermissionDeniedResponse();
 });
 
 // tenant routes
 router.group('/v1/t', (r) => {
   registerTenantController(r)
-}).registerMiddleware((req, next) => {
-  const token = getAccessToken(req);
-  req.addData('token', token);
-  req.addData('tenant', { id: 123, name: 'foo' }); // TODO: Change to real data
-
-  return next(req)
+}).registerMiddleware(async (req, next) => {
+  return (isAdmin(await enrichRequest(req))) ? next(req) : makePermissionDeniedResponse();
 })
 
 
 // uploading
-router.post("/v1/f", uploadController, 'handleUploadedFile'); // TODO: Add auth middleware
+router.post("/v1/f", uploadController, 'handleUploadedFile').registerMiddleware(async (req, next) => {
+  return (canUpload(await enrichRequest(req))) ? next(req) : makePermissionDeniedResponse();
+});
 
 // serving
 router.get('/s/:id', serverController, 'serveFile');
