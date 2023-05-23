1bde77e4663c65479862b0db79260650dfa06206
Jibao Mansaray <jibaomansaray@gmail.com>
2023-05-23 15:03:57 +1000

    change date fields to numeric
    This seems to be wrong still

--- backend/setup/sql.ts
+++ backend/setup/sql.ts
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS "tenants" (
 	"status"	INTEGER NOT NULL,
 	"is_default"	INTEGER NOT NULL,
 	"name"	INTEGER NOT NULL,
-	"created_at"	INTEGER NOT NULL DEFAULT 0,
-	"updated_at"	INTEGER DEFAULT 0,
-	"deleted_at"	INTEGER DEFAULT 0,
+	"created_at"	NUMERIC NOT NULL DEFAULT 0,
+	"updated_at"	NUMERIC DEFAULT 0,
+	"deleted_at"	NUMERIC DEFAULT 0,
 	PRIMARY KEY("internal_id" AUTOINCREMENT)
 )`;
 
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"name"	TEXT,
 	"status"	INTEGER NOT NULL,
 	"system_admin"	INTEGER NOT NULL,
-	"created_at"	INTEGER NOT NULL DEFAULT 0,
-	"updated_at"	INTEGER DEFAULT 0,
-	"deleted_at"	INTEGER DEFAULT 0,
+	"created_at"	NUMERIC NOT NULL DEFAULT 0,
+	"updated_at"	NUMERIC DEFAULT 0,
+	"deleted_at"	NUMERIC DEFAULT 0,
 	PRIMARY KEY("internal_id" AUTOINCREMENT)
 )
 `;
@@ -32,9 +32,9 @@ CREATE TABLE IF NOT EXISTS "user_tenants" (
 	"status"	INTEGER NOT NULL,
 	"roles"	TEXT NOT NULL,
 	"token"	TEXT NOT NULL,
-	"created_at"	INTEGER NOT NULL,
-	"updated_at"	INTEGER,
-	"deleted_at"	INTEGER,
+	"created_at"	NUMERIC NOT NULL,
+	"updated_at"	NUMERIC DEFAULT 0,
+	"deleted_at"	NUMERIC DEFAULT 0,
 	FOREIGN KEY("user_internal_id") REFERENCES "users"("internal_id"),
 	FOREIGN KEY("tenant_internal_id") REFERENCES "tenants"("internal_id")
 )
