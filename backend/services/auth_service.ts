e3145e61f3dc9c6ac6dc80140af8fecd62a33b7b
Jibao Mansaray <jibaomansaray@gmail.com>
2023-05-24 17:25:25 +1000

    Add functions that use instance of the `user` entity

--- backend/services/auth_service.ts
+++ backend/services/auth_service.ts
@@ -1,6 +1,7 @@
 import { userTenantRepo } from "../app.ts";
 import { HTTPRequest } from "../core/router.ts";
-import { ITenant, TenantId } from "../entities/tenant_entity.ts";
+import { Tenant, TenantId } from "../entities/tenant_entity.ts";
+import { User } from "../entities/user_entity.ts";
 import { UserTenant, UserTenantRole, UserTenantStatus } from "../entities/user_tenant_entity.ts";
 import { ApiError, makeApiHttpResponse } from "./api_service.ts";
 
@@ -43,6 +44,24 @@ export function isAdmin (userTenant: UserTenant | false): boolean {
   return userTenant.roles.indexOf(UserTenantRole.ADMIN) > -1 || isSysAdmin(userTenant)
 }
 
+export async function userIsAdmin (user: User | number, tenant: Tenant | number): Promise<boolean> {
+  const result = await userTenantRepo.findUserByUserAndTenant(user, tenant)
+  if (result.success) {
+    return isAdmin(result.data);
+  }
+
+  return false;
+}
+
+export async function userCanUpload (user: User | number, tenant: Tenant | number): Promise<boolean> {
+  const result = await userTenantRepo.findUserByUserAndTenant(user, tenant)
+  if (result.success) {
+    return canUpload(result.data);
+  }
+
+  return false;
+}
+
 export function canUpload (userTenant: UserTenant | false): boolean {
   if (!userTenant) {
     return false;
@@ -56,7 +75,7 @@ export function isGuest (userTenant: UserTenant | false): boolean {
   return userTenant === false;
 }
 
-export function generateUserToken (tenant: ITenant | TenantId): string {
+export function generateUserToken (tenant: Tenant | TenantId): string {
   const id = (typeof tenant === 'object') ? tenant.id as TenantId : tenant;
   return `${id}.${crypto.randomUUID().replaceAll('-', '')}`
 }
